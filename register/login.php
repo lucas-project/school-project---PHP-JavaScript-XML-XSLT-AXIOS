a58ba7b371de95e2d0f16881d14e71f057da7dec
Lucas-Qin <niki.grey.uk@gmail.com>
2022-10-09 22:46:02 +1100

    4.1 done

--- register/login.php
+++ register/login.php
@@ -30,18 +30,31 @@ if(isset($_GET["email"]) && isset($_GET["password"])) {
         //$proc->importStyleSheet($xslDoc);
         $emails = $xmlDoc->getElementsByTagName('email');
         $passwords = $xmlDoc->getElementsByTagName('password');
+        $ids = $xmlDoc->getElementsByTagName('id');
         $length = $emails->length;
+        $errormsg = array();
+        $correctmsg = array();
         for ($i = 0; $i < $length; $i++) {
             $emailValue = $emails->item($i)->nodeValue;
             $passValue = $passwords->item($i)->nodeValue;
             if (trim($emailValue) == trim($email) && trim($passValue) == trim($password)) {
-                echo "successfully logined";
-                $_SESSION["useremail"] = $email;
+                echo "successfully logined<br>";
+                $idValue = $ids->item($i)->nodeValue;
+                $_SESSION["userid"] = $idValue;
+                $_SESSION["email"]=$emailValue;
+                echo "Your email address is ".$_SESSION['email'].", and you customer id is ".$_SESSION['userid']."<br>";
+                echo "<br><a href='./buying/buying.htm'>buuying page</a>";
+                array_push($correctmsg,"successfully");
                 break;
             } else {
-                echo "Email or password incorrect, please try again";
+                array_push($errormsg, "Email or password is incorrect, please try again");
             }
+
+
 //        echo $proc->transformToXML($xmlDoc);
+        }
+        if (count($correctmsg)==0){
+            echo $errormsg[0];
         }
     }
 }
